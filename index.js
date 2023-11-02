157a3602c7e80794814f4f642583303e8952ca29
Sarang Bang <bangsarang7@gmail.com>
2023-11-02 15:36:35 +0530

    nwq

---
+++ index.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGnMBeUT1IRGZF6zzUCCk6RGW0F-XHLo0",
+    authDomain: "myformschool.firebaseapp.com",
+    projectId: "myformschool",
+    storageBucket: "myformschool.appspot.com",
+    messagingSenderId: "1068038485266",
+    appId: "1:1068038485266:web:0da735399c7a44a672efcb",
+    measurementId: "G-EJ9168FTXM"
+  };
